94c5e425fe144b1b4d58318bde17e6e745202a9d
Locus_Natit <3113135718@qq.com>
2022-12-17 15:38:47 +0800

    fix a game-crashing bug

--- src/main/java/com/lnatit/enchsort/EnchSort.java
+++ src/main/java/com/lnatit/enchsort/EnchSort.java
@@ -57,6 +57,7 @@ public class EnchSort
         final ItemStack stack = event.getItemStack();
 
         boolean forSort = stack.isEnchanted() || (EnchSortConfig.ALSO_SORT_BOOK.get() && stack.getItem() instanceof EnchantedBookItem);
+        forSort = forSort || (getHideFlags(stack) & ItemStack.TooltipDisplayFlags.ENCHANTMENTS.getMask()) == 0;
         if (!forSort || event.getEntity() == null)
             return;
 
@@ -86,6 +87,11 @@ public class EnchSort
                     break;
             }
         }
+        if (index + enchs.size() > toolTip.size())
+        {
+            LOGGER.warn("Some tooltip lines are missing!!!");
+            return;
+        }
         toolTip.subList(index, index + enchs.size()).clear();
 
         // Sort the enchMap & generate toolTip
@@ -94,5 +100,10 @@ public class EnchSort
         enchArray.sort(EnchSortConfig.EnchComparator.getInstance());
         for (Map.Entry<Enchantment, Integer> entry : enchArray)
             toolTip.add(index++, EnchSortConfig.getFullEnchLine(entry));
+    }
+
+    private static int getHideFlags(ItemStack stack)
+    {
+        return stack.hasTag() && stack.getTag().contains("HideFlags", 99) ? stack.getTag().getInt("HideFlags") : 0;
     }
 }
